3de99e33c9049651eaaf9db7aec52eeda88800da
Wes Kelly <weskelly@mac.com>
2015-10-30 12:50:58 -0400

    Added a demo that has the sample specified in the project description.

--- RushHour/src/rushhour/RushHour.java
+++ RushHour/src/rushhour/RushHour.java
@@ -1,6 +1,7 @@
+
 /**
- * Title : Project 3, CS3410 
- * Description: A breadth-first-search solution to the Rush Hour game.
+ * Title : Project 3, CS3410 Description: A breadth-first-search solution to the
+ * Rush Hour game.
  *
  * Copyright : Copyright (c) 2015 Wesley Kelly, James Von Eiff, Cedarville
  * University
@@ -12,29 +13,45 @@
 package rushhour;
 
 import java.awt.Color;
+
 public class RushHour
 {
 
     /**
      * @param args the command line arguments
+     * @throws rushhour.InvalidMovementException
      */
-    public static void main(String[] args) throws InvalidMovementException
+    public static void main(String[] args)
+        throws InvalidMovementException
     {
         final GameBoard board = new GameBoard();
-        
+
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
             @Override
-            public void run() {
-               board.setVisible(true);
+            public void run()
+            {
+                board.setVisible(true);
             }
         });
-        
-        Vehicle vehicle = new Vehicle(Vehicle.Type.car, Color.red, 1, 2, Vehicle.Orientation.horizontal);
-        
-        board.drawOneVehicle(vehicle);
-        vehicle.right();
-        board.drawOneVehicle(vehicle);
-        
+
+        Vehicle red = new Vehicle(Vehicle.Type.car, Color.red, 1, 2, Vehicle.Orientation.horizontal);
+        Vehicle lime = new Vehicle(Vehicle.Type.car, Color.green, 0, 0, Vehicle.Orientation.horizontal);
+        Vehicle purple = new Vehicle(Vehicle.Type.truck, Color.magenta, 0, 1, Vehicle.Orientation.vertical);
+        Vehicle orange = new Vehicle(Vehicle.Type.car, Color.orange, 0, 4, Vehicle.Orientation.vertical);
+        Vehicle blue = new Vehicle(Vehicle.Type.truck, Color.blue, 3, 1, Vehicle.Orientation.vertical);
+        Vehicle yellow = new Vehicle(Vehicle.Type.truck, Color.yellow, 5, 0, Vehicle.Orientation.vertical);
+        Vehicle lightBlue = new Vehicle(Vehicle.Type.car, Color.cyan, 4, 4, Vehicle.Orientation.horizontal);
+        Vehicle aqua = new Vehicle(Vehicle.Type.truck, Color.lightGray, 2, 5, Vehicle.Orientation.horizontal);
+
+        board.addVehicle(red);
+        board.addVehicle(lime);
+        board.addVehicle(purple);
+        board.addVehicle(orange);
+        board.addVehicle(blue);
+        board.addVehicle(yellow);
+        board.addVehicle(lightBlue);
+        board.addVehicle(aqua);
     }
 }
